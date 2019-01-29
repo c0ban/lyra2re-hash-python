34cfe16afb28b535a1addada27705f2c6972791b
c0ban-project <39722780+c0ban-project@users.noreply.github.com>
2019-01-29 11:27:28 +0900

    Add lyra2rec0ban_hash

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import setup, Extension
 
+lyra2rec0ban_hash_module = Extension('lyra2rec0ban_hash',
+                               sources = [
+										  'lyra2rec0banmodule.c',
+                                          'Lyra2RE.c',
+										  'Sponge.c',
+										  'Lyra2.c',
+										  'sha3/blake.c',
+										  'sha3/groestl.c',
+										  'sha3/keccak.c',
+										  'sha3/cubehash.c',
+										  'sha3/bmw.c',
+										  'sha3/skein.c'],
+                               include_dirs=['.', './sha3'])
+
 lyra2re2_hash_module = Extension('lyra2re2_hash',
                                sources = [
 										  'lyra2re2module.c',
@@ -29,11 +43,10 @@ lyra2re_hash_module = Extension('lyra2re_hash',
                                include_dirs=['.', './sha3'])
 
 
-setup (name = 'lyra2re2_hash',
-       version = '1.1.2',
-       author_email = 'jameslovejoy1@gmail.com',
-       author = 'James Lovejoy',
-       url = 'https://github.com/metalicjames/lyra2re-hash-python',
-       description = 'Bindings for Lyra2RE2 proof of work used by Vertcoin',
-       ext_modules = [lyra2re2_hash_module, lyra2re_hash_module])
-
+setup (name = 'lyra2rec0ban_hash',
+       version = '1.0.0',
+       author_email = 'c0ban8project@gmail.com',
+       author = 'c0ban project',
+       url = 'https://github.com/c0ban/lyra2re-hash-python',
+       description = 'Bindings for Lyra2REc0ban proof of work used by c0ban',
+       ext_modules = [lyra2rec0ban_hash_module, lyra2re2_hash_module, lyra2re_hash_module])
